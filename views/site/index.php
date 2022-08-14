ee0b613b17e24d7597404dfe2d9a1d1c79f3edd7
Роман Ефименков <denfima14@yandex.ru>
2022-08-15 00:36:20 +0300

    change

--- views/site/index.php
+++ views/site/index.php
@@ -3,7 +3,7 @@
 /** @var yii\web\View $this */
 $this->title = 'My Yii Application';
 ?>
-<h1 class="main_text">Тестовое Задание</h1>
+<h1 class="main_text">Тестовое задание</h1>
 <div style="text-align: center">
     <iframe src="https://drive.google.com/file/d/1t3dQIUePV8Id3z688ZIYQUQ998y-tISc/preview" width="640" height="480" allow="autoplay"></iframe>
 </div>
